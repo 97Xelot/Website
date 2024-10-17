55e81a0a5ef599a9d3651f98e50c7ecbc53d04f5
Nkosiphendule <108423440+97Xelot@users.noreply.github.com>
2024-10-17 15:19:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let count = 0;
+setInterval(() => {
+    console.log(count);
+},1)
+
+const findSA = (arr) =>{
+    for (let index= 0; index < arr.length; index++){
+        const Element = arr[index];
+        if(Element.cca2 === "ZA")
+            return Element;
+    }
+}
+
+const ChangeJohn = (data) =>{
+    const SountAH = findSA(data);
+    document.getElementById("profile-pic").src = SountAH.flags.png
+}
+
+const data = fetch('https://restcountries.com/v3.1/all').
+then((response) =>{ 
+    console.log(response);
+    response.json().then(data =>{
+        console.log(data);
+        ChangeJohn(data);
+    });
+})
